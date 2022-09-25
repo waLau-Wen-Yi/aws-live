537bc77a530318fd563587fb0d96bd935c333395
waLau-Wen-Yi <78148201+waLau-Wen-Yi@users.noreply.github.com>
2022-09-26 00:44:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cjbhbslfawbp.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lauwenyi-employee"
+customregion = "us-east-1b"
