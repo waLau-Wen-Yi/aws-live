c85beaf99ef2b2b846e8192fdca4c3461505cb56
waLau-Wen-Yi <78148201+waLau-Wen-Yi@users.noreply.github.com>
2022-09-27 00:30:15 +0800

    test

--- EmpApp.py
+++ EmpApp.py
@@ -33,6 +33,7 @@ def about():
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
     emp_id = request.form['emp_id']
+    print(emp_id)
     return render_template('[!]ShowEmpDetails.html', id=emp_id)
 
 @app.route("/shwempcrdhoz", methods=['GET', 'POST'])
