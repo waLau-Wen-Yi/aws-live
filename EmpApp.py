95dd1ac73f5a04392be9f4fc6fa664ab1ad58456
waLau-Wen-Yi <78148201+waLau-Wen-Yi@users.noreply.github.com>
2022-09-27 14:51:00 +0800

    as

--- EmpApp.py
+++ EmpApp.py
@@ -35,6 +35,61 @@ def about():
 def EmpMng():
     return render_template('/EmpMng/[!]EmpMngHome.html')
 
+
+@app.route("/rgsemp", methods=['GET', 'POST'])
+def RgsEmp():
+    return render_template('/EmpMng/[!]RegisEmp.html')
+
+@app.route("/addemp", methods=['POST'])
+def AddEmp():
+        emp_id = request.form['emp_id']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        pri_skill = request.form['pri_skill']
+        location = request.form['location']
+        emp_image_file = request.files['emp_image_file']
+
+        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+        cursor = db_conn.cursor()
+
+        if emp_image_file.filename == "":
+            return "Please select a file"
+
+        try:
+
+            cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+            db_conn.commit()
+            emp_name = "" + first_name + " " + last_name
+            # Uplaod image file in S3 #
+            emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+            s3 = boto3.resource('s3')
+
+            try:
+                print("Data inserted in MySQL RDS... uploading image to S3...")
+                s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+                bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+                s3_location = (bucket_location['LocationConstraint'])
+
+                if s3_location is None:
+                    s3_location = ''
+                else:
+                    s3_location = '-' + s3_location
+
+                object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                    s3_location,
+                    custombucket,
+                    emp_image_file_name_in_s3)
+
+            except Exception as e:
+                return str(e)
+
+        finally:
+            cursor.close()
+
+        print("all modification done...")
+        return render_template('AddEmpOutput.html', name=emp_name)
+
+
 @app.route("/shwempdtl", methods=['GET', 'POST'])
 def ShwEmpDtl():
     routePage = "/EmpMng/[!]ShowEmpDetails.html"
@@ -91,58 +146,24 @@ def EdtEmpDtl():
              )
     return render_template(routePage)
 
-@app.route("/rgsemp", methods=['GET', 'POST'])
-def RgsEmp():
-    return render_template('/EmpMng/[!]RegisEmp.html')
-
-@app.route("/addemp", methods=['POST'])
-def AddEmp():
-        emp_id = request.form['emp_id']
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        pri_skill = request.form['pri_skill']
-        location = request.form['location']
-        emp_image_file = request.files['emp_image_file']
-
-        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-        cursor = db_conn.cursor()
-
-        if emp_image_file.filename == "":
-            return "Please select a file"
-
-        try:
-
-            cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-            db_conn.commit()
-            emp_name = "" + first_name + " " + last_name
-            # Uplaod image file in S3 #
-            emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-            s3 = boto3.resource('s3')
-
-            try:
-                print("Data inserted in MySQL RDS... uploading image to S3...")
-                s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-                bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-                s3_location = (bucket_location['LocationConstraint'])
-
-                if s3_location is None:
-                    s3_location = ''
-                else:
-                    s3_location = '-' + s3_location
-
-                object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                    s3_location,
-                    custombucket,
-                    emp_image_file_name_in_s3)
-
-            except Exception as e:
-                return str(e)
-
-        finally:
-            cursor.close()
-
-        print("all modification done...")
-        return render_template('AddEmpOutput.html', name=emp_name)
+@app.route('/rmvemp', methods=['GET', 'POST'])
+def RmvEmp():
+    routePage = "/EmpMng/[!]RemoveEmp.html"
+    cursor = db_conn.cursor()
+    if (request.method == 'GET'):
+        emp_id = request.args['emp_id']
+        qryRslt = cursor.execute("SELECT * FROM employee WHERE id = (%s)", (emp_id))
+        if qryRslt == 0:
+            return render_template(routePage, id = "DATA NOT FOUNDED, PLEASE SEARCH ANOTHER ID")
+        else:
+            empData = cursor.fetchall()
+            return render_template(routePage,
+             fname = empData[0][2],
+             lname = empData[0][3],
+             position = empData[0][4],
+             jdate = empData[0][7]
+             )
+    return render_template(routePage)
 
 #@@@@@@@@@@Performance Tracker
 
