6c53b46e36d6e016cb240be1d85fa52be2f14bc0
waLau-Wen-Yi <78148201+waLau-Wen-Yi@users.noreply.github.com>
2022-09-28 12:27:55 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -56,7 +56,7 @@ def AddEmp():
         emp_skills = request.form['emp_skills']
         emp_image_file = request.files['image_file']
 
-        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         cursor = db_conn.cursor()
 
         if emp_image_file.filename == "":
